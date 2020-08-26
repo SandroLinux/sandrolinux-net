599fe1643b5d8961c6e05f1904b2886193d6cd6d
SandroLinux <69315452+SandroLinux@users.noreply.github.com>
2020-08-26 15:56:55 +0100

    Add files via upload

--- pwa.js
+++ pwa.js
@@ -2,9 +2,20 @@ if ("serviceWorker" in navigator){
   navigator.serviceWorker.register("sw.js").then(registration => {
     console.log("SW Registered!");
     console.log(registration);
-  }).catch(error =>{
-    console.log("SW Registeration Failed!");
+  }).catch(error => {
+    console.log("SW Registration Failed!");
     console.log(error);
   });
 }
+function updateSubscriptionOnServer(subscription) {
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails =
+    document.querySelector('.js-subscription-details');
 
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
+}
