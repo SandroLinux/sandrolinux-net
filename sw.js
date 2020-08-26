51e652c42229c62e16aea1b9a4a6552105c8ee46
SandroLinux <69315452+SandroLinux@users.noreply.github.com>
2020-08-26 10:36:57 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e =>{
   e.waitUntil(
     caches.open("static").then(cache => {
-      return cache.addAll(["https://www.sandrolinux.com/", "https://www.sandrolinux.com/mainpage.html", "https://www.sandrolinux.com/sandrolinux.css", "https://www.sandrolinux.com/privacypolicy.html", "https://www.sandrolinux.com/allstories.html", "https://www.sandrolinux.com/PWA%20Icon%20180.jpg"]);
+      return cache.addAll(["https://www.sandrolinux.com/", "https://www.sandrolinux.com/mainpage.html", "https://www.sandrolinux.com/sandrolinux.css", "https://www.sandrolinux.com/privacypolicy.html", "https://www.sandrolinux.com/news/allstories.html", "https://www.sandrolinux.com/PWA%20Icon%20180.jpg", "https://www.sandrolinux.com/myplaylist.html"]);
     })
     );
 });
